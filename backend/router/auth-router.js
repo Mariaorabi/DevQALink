81d939f15deba5c88f0d15e248a06b3969f6ba61
AzizHamed <Abd.Elaziz.Hamed@e.braude.ac.il>
2024-09-04 12:09:04 +0300

    update auth-router

--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var userController = require('../controllers/auth-controller');
 
 router.get('/',userController.getAllUsers)// localhost:3000/user/
-router.post('/getuser',userController.getUser)
+router.post('/getuser',userController.getUser) // localhost:3000/auth/getuser
 router.post('/signup',userController.postUser); // localhost:3000/user/signup
 // router.put('/updatest',userController.updateStudent);// localhost:3000/user/updatest
 // router.put('/updatest/:age',userController.updatenewStudent);// localhost:3000/user/updatest
