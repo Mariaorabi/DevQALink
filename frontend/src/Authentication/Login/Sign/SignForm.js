b9cafb90689dc6cd977304035ee9b14294709598
AzizHamed <Abd.Elaziz.Hamed@e.braude.ac.il>
2024-09-09 16:18:07 +0300

    add face recognition

--- frontend/src/Authentication/Login/Sign/SignForm.js
+++ frontend/src/Authentication/Login/Sign/SignForm.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+/* global faceIO */
+import React, { useEffect, useState } from 'react'
 
 import dell from '../../../images/dell.webp'
 import Login from './Login'
 import Signup from './Signup'
+import { useDispatch } from 'react-redux'
+import { setFaceIo } from '../../../Utility/Redux/Slices/FaceIoSlice'
 const SignForm = (props) => {
+
+
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setFaceIo(new faceIO("fioac3b3")))
+      // faceio = new faceIO("fioac3b3");
+  }, []);
+
   const [loginInput, setloginInput] = useState({})
   return (
     <div id='leftSide' className={props.sign ? 'SignInSlideRIGHT' : 'SignupSlideLeft'}>
@@ -12,7 +25,7 @@ const SignForm = (props) => {
 
 
 
-{props.sign ? <Signup/> : <Login/>}
+{props.sign ? <Signup /> : <Login />}
 
    
 
--- frontend/src/Utility/Redux/store.js
+++ frontend/src/Utility/Redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import AuthReducer from './Slices/AuthSlice'
+import FaceIoReducer from './Slices/FaceIoSlice'
 export const store = configureStore({
     reducer: {
       auth : AuthReducer,
+      faceIo : FaceIoReducer
         // Add reducers here
     }
 })
